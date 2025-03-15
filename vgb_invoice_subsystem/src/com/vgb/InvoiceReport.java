0ab10b30c610872b1e724056185f3d66d42ccd42
Ermias Wolde <Ewolde2@huskers.unl.edu>
2025-03-14 21:16:14 -0500

    Added invoiceDetailedSummary and customerSummary methods to the InvoiceReport.java

--- vgb_invoice_subsystem/src/com/vgb/InvoiceReport.java
+++ vgb_invoice_subsystem/src/com/vgb/InvoiceReport.java
@@ -19,8 +19,45 @@ public class InvoiceReport {
 //		The first report will give a summary of all invoices along with a few totals.
 
 //		The second report will give a similar summary but for each customer.
+public static void customerSummary(List<Invoice> invoices) {
+    Map<String, Total> customerTotals = new HashMap<>();
+        
+		for (Invoice invoice : invoices) {
+        	String customerName = invoice.getCustomer().getFirstName() + " " + invoice.getCustomer().getLastName();
+        	customerTotals.putIfAbsent(customerName, Total.empty());
+        	customerTotals.get(customerName).add(invoice.getTotal());
+    }
+        
+    System.out.println("Customer Summary:");
+        for (Map.Entry<String, Total> entry : customerTotals.entrySet()) {
+            System.out.println("Customer: " + entry.getKey());
+            System.out.println("Subtotal: $" + entry.getValue().getCost());
+            System.out.println("Tax Total: $" + Round.toCents(entry.getValue().getTax()));
+            System.out.println("Grand Total: $" + entry.getValue().getTotal());
+            System.out.println();
+        }
+    }
+    
 
 //		The final report will give details for each individual invoice.
+	public static void invoiceDetailedSummary(List<Invoice> invoices) {
+    System.out.println("Invoice Detailed Summary:");
+        for (Invoice invoice : invoices) {
+            System.out.println("Invoice UUID: " + invoice.getUuid());
+            System.out.println("Customer: " + invoice.getCustomer().getFirstName() + " " + invoice.getCustomer().getLastName());
+            System.out.println("Salesperson: " + invoice.getSalesperson().getFirstName() + " " + invoice.getSalesperson().getLastName());
+            System.out.println("Items:");
+            
+        for (Item item : invoice.getItems()) {
+            System.out.println("  - " + item.getName() + " | Price: $" + item.getPrice() + " | Quantity: " + item.getQuantity());
+        }
+            
+        System.out.println("Subtotal: $" + invoice.subtotal());
+        System.out.println("Tax Total: $" + invoice.taxTotal());
+        System.out.println("Grand Total: $" + invoice.grandTotal());
+        System.out.println("------------------------------");
+        }
+    }
 	}
 	
 	public static void invoicesSummary(List<Invoice> invoices) {
@@ -41,11 +78,4 @@ public class InvoiceReport {
 
 	}
 	
-	public void customerSummary(List<Invoice> invoices) {
-		
-	}
-	
-	public void invoiceDetailedSummary(List<Invoice> invoices) {
-		
-	}
 }
